0df0818c4129d1efd358420dea56918d89a63485
Vinay Yadav <itsvinayadav@gmail.com>
2024-10-09 17:25:59 +0530

    removed console

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
-console.log(registerUser);
 const router = Router();
 
 router.route("/register").post(registerUser);
